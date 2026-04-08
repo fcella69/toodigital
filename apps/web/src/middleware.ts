6bd27970023d1270dcf015202e89b057871394fa
LAPTOP-D0RGEHRP\Francesco <francesco.cella.6991@gmail.com>
2026-04-08 19:06:45 +0200

    Disabled Middleware.ts e sito online

--- apps/web/src/middleware.ts
+++ apps/web/src/middleware.ts
@@ -1,9 +1,15 @@
 import { NextRequest, NextResponse } from "next/server";
 
+const AUTH_ENABLED = false;
+
 const USER = process.env.BASIC_AUTH_USER;
 const PASS = process.env.BASIC_AUTH_PASS;
 
 export function middleware(req: NextRequest) {
+  if (!AUTH_ENABLED) {
+    return NextResponse.next();
+  }
+
   // Se non sono definite le credenziali → lascia passare
   if (!USER || !PASS) {
     return NextResponse.next();
